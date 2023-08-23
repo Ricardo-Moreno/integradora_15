20d95cf7f68735283a4438236991dc8847745154
Ricardo Moreno <ricardo.yvl@gmail.com>
2023-08-22 23:21:42 -0300

    variable

--- src/app.js
+++ src/app.js
@@ -1,67 +1,53 @@
 import express from 'express';
-import { Server } from "socket.io"
-import handlebars from "express-handlebars"
-import __dirname from "./utils/utils.js";
-import cartsRoutes from './routes/carts.routes.js';
+import { Server } from "socket.io";
+import handlebars from "express-handlebars";
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'; // Importa dotenv para cargar las variables de entorno
 
+import cartsRoutes from './routes/carts.routes.js';
+import messageRoutes from './routes/message.routes.js';
 
-import messageRoutes from './routes/message.routes.js'
+dotenv.config(); // Carga las variables de entorno del archivo .env
 
-
-
-const app = express()
+const app = express();
 const PORT = 8080;
-const MONGO = 'mongodb+srv://ricardoyvl:1UAObqb6Jwwo6dI3@ecommerce.stcb13i.mongodb.net/ecommerce'
-const connection = mongoose.connect(MONGO)
 
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // ... otras opciones de configuración de mongoose
+});
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
+app.engine('handlebars', handlebars());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
-// app.engine definimos un motor para la aplicacion
-app.engine('handlebars', handlebars.engine());
-//las plantillas van a estar alojadas en la carpeta view. CONFICORAMOS
-app.set('views', __dirname + '/views')
-// Ahora definimos el motor que vamos a usar en esas vistas
-app.set('view engine', 'handlebars')
-
-app.use('/', messageRoutes)
-app.use('/carts', cartsRoutes)
-
-
-
+app.use('/', messageRoutes);
+app.use('/carts', cartsRoutes);
 
 const server = app.listen(PORT, () => {
-    console.log('Servidor Funcionando en el Puerto' + PORT)
-})
-const io = new Server(server)
+    console.log('Servidor Funcionando en el Puerto ' + PORT);
+});
 
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
+const io = new Server(server);
 
 const logs = [];
 
 io.on('connection', socket => {
-    console.log("connected")
+    console.log("Conectado");
 
-    socket.emit('log', { logs })
+    socket.emit('log', { logs });
 
     socket.on('message1', data => {
-        io.emit('log', data)
-    })
+        io.emit('log', data);
+    });
 
     socket.on('message2', data => {
-        logs.push({ socketid: socket.id, message: data })
-        io.emit('log', { logs })
-    })
-})
-
-
-
-
-
-
-
+        logs.push({ socketid: socket.id, message: data });
+        io.emit('log', { logs });
+    });
+});
